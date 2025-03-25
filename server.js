9ede50b8873cbe79e177ed5f34f0e4aaedeee618
LuisFelipeQuin <felipemorenoquin@gmail.com>
2025-03-25 15:30:40 -0400

    testing

--- server.js
+++ server.js
@@ -14,7 +14,13 @@ const setupWebRTCSignaling = require('./webrtcHandlers');
 
 // CORS and middlewares
 const corsOptions = {
-  origin: ['http://localhost:3000', 'http://ec2-3-137-181-105.us-east-2.compute.amazonaws.com:3000/', "http://3.137.181.105:3000/"],
+  // origin: ['http://localhost:3000', 'http://ec2-3-137-181-105.us-east-2.compute.amazonaws.com:3000/', "http://3.137.181.105:3000/"],
+  origin: [
+    'http://localhost:3000',
+    'http://ec2-3-137-181-105.us-east-2.compute.amazonaws.com:3000/',
+    'http://3.137.181.105:3000/',
+    'http://ec2-18-188-236-31.us-east-2.compute.amazonaws.com:3000/' // Add the actual domain here
+  ],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: 'Content-Type,Authorization',
   credentials: true
